e261c50040a18e10b85a7b7f44cf577959dff0c0
Akhator Itua <iakhator@gmail.com>
2020-07-02 18:20:59 +0100

    set tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com"></input>
+      <Nav />
+      <div className="container">
+        <input
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="email"
+          placeholder="jane@example.com"
+        ></input>
+      </div>
     </div>
   );
 }
